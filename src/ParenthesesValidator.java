e411b112cead7703d010d6dcd94b300c89cfe0f2
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-23 14:30:21 +0400

    Task 5: Validate Parentheses and Balanced Expressions Using Java Stack

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -1,2 +1,47 @@
+import java.util.Stack;
+import java.util.Iterator;
+
 public class ParenthesesValidator {
-}
+    public static void main(String[] args) {
+
+    }
+
+    public ParenthesesValidator(String Stack , String exp , int i) {
+
+
+
+        Stack<String> strings   = new Stack<>();
+
+        strings.push("()");
+        strings.push("(())");
+        strings.push("({[]})");
+        strings.push("(()");
+        strings.push("([)]");
+
+
+
+        for(int i =0; i<exp.len();  i++);
+
+        char ch = exp.charAt(i);
+
+        if(ch=='(' || ch=='[' || ch=='{' ){
+            strings.push("ch");
+        }
+        if(Stack.isEmpty()){
+            return false;
+
+            char top = strings.pop();
+
+            if (!((top == '(' && ch == ')') ||
+                    (top == '[' && ch == ']') ||
+                    (top == '{' && ch == '}'))) {
+
+                for (String s : strings) {
+                    System.out.println(s);
+                }
+            }
+        }
+            }
+        }
+
+
